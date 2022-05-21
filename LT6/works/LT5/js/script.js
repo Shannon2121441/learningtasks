ef124d7f8f1a2c1b3bd4f1a45ab8cf7063df724f
Shannon2121441 <99891185+Shannon2121441@users.noreply.github.com>
2022-05-21 22:53:51 +0800

    Add files via upload

---
+++ LT6/works/LT5/js/script.js
@@ -0,0 +1,29 @@
+function lyre(){
+        document.getElementById('myImage').src="images/wink_venti.png";
+        document.getElementById('dialogue').innerHTML="Of course. I'm the best bard in the world. There's not a single song I do not know, no matter if it's from the past, present, or future. Look me in the eyes. Do you not find me trustworthy?";
+}
+
+function glider(){
+        document.getElementById('myImage').src="images/excited_venti.png";
+        document.getElementById('dialogue').innerHTML="Ooooh~ A wind glider! Come on Traveler, let's go! The world is full of lost ballads just waiting to be rediscovered.";
+}
+
+function cecilia(){
+        document.getElementById('myImage').src="images/happy_venti.png";
+        document.getElementById('dialogue').innerHTML="Traveler, have you ever seen a cecilia? It's a magnificent white wildflower that only grows on the most remote mountains and clifftops. To me, at least, it is the most beautiful flower in all of Teyvat.";
+}
+
+function wish(){
+        document.getElementById('myImage').src="images/idea_venti.png";
+        document.getElementById('dialogue').innerHTML="My greatest wish? It has always been to roam free and experience the whole world. Now I would add that wherever I go, it simply must be with you! Each day with you is an adventure, and where adventurers go, storytellers must follow!";
+}
+
+function Zhongli(){
+        document.getElementById('myImage').src="images/shocked_venti.png";
+        document.getElementById('dialogue').innerHTML="Have you seen that gentleman around? Huh? He's just a normal man by the name of Zhongli now? That must be quite the change for that old block-head. Did he still seem strong when you saw him? Am I likely to get blown away?";
+}
+
+function Celestia(){
+        document.getElementById('myImage').src="images/sigh_venti.png";
+        document.getElementById('dialogue').innerHTML="Celestia... I'm not sure even I could fly that far. In any case, the water there tastes foul and the fruit is bland. You know what that means? No cider! Haha, in that case, I wouldn't go there even if I was invited.";
+}
