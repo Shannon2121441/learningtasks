ccc3940d901f15b5fde026642f8d654e3f814a61
Shannon2121441 <99891185+Shannon2121441@users.noreply.github.com>
2022-05-21 23:14:11 +0800

    Add files via upload

---
+++ nav/works.php
@@ -0,0 +1,47 @@
+    <?php
+        echo "<center><b>Previous Works</b></center>";
+    ?>
+    <ul>
+        <li><a href="../LT2/Recipes.html" target="_blank"><b>Learning Task 2.1</b></a></li>
+        <li><a href="../LT3/index.html" target="_blank"><b>Learning Task 3.1</b></a></li>
+        <li><a href="../CLE1/index.html" target="_blank"><b>CLE1 (Midterm)</b></a></li>
+        <li><a href="../LT4/index.html" target="_blank"><b>Learning Task 4.1</b></a></li>
+        <li><a href="../LT5/index.html" target="_blank"><b>Learning Task 5.1</b></a></li>
+    </ul>
+    
+    <div id="desc">
+        <?php
+        echo "<u>Online Recipe Web Site</u><br>";
+        echo "<b>Goal:</b> allow the student to familiarize themselves with the HTML document, its form, and tags<br><br>";
+        echo "<b>Output:</b> develop a simple website with seven different webpages containing  recipes";
+        ?>
+    </div>
+    <div id="desc">
+        <?php
+        echo "<u>My Top 10 Favorites</u><br><br>";
+        echo "<b>Goal:</b> allow students to practice with HTML more and learn how to design it using CSS<br><br>";
+        echo "<b>Output:</b> create a web page showing the students' Top 10 Favorites from certain categories";
+        ?>
+    </div>
+    <div id="desc">
+        <?php
+        echo "<u>Midterm Exam</u><br><br>";
+        echo "<b>Goal:</b> develop students' skills in content gathering and generation, HTML markup writing, and CSS<br><br>";
+        echo "<b>Output:</b> create a 4 page web site that uses basic HTML document markups and CSS";
+        ?>
+    </div>
+    <div id="desc">
+        <?php
+        echo "<u>Web Typography</u><br><br>";
+        echo "<b>Goal:</b> allow students to practice proper CSS text formatting and font family declarations<br><br>";
+        echo "<b>Output:</b> create a one page HTML document using a poem or quotation as content";
+        ?>
+    </div>
+    <div id="desc">
+        <?php
+        echo "<u>JavaScript HTML DOM Events</u><br>";
+        echo "<b>Goal:</b> allow students to practice with CSS more and learn how to add JavaScript actions<br><br>";
+        echo "<b>Output:</b> create a simple interactive webpage that reacts to the actions of the user";
+        ?>
+    </div>
+
